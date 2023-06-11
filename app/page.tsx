76a73cf2c67c995af7b916a255ec01da94cb1f59
Hung VQ <hungvq.dev@gmail.com>
2023-06-12 01:20:21 +0700

    Show More

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { CarCard, CustomFilter, Hero, SearchBar } from "../components";
+import {
+  CarCard,
+  CustomFilter,
+  Hero,
+  SearchBar,
+  ShowMore,
+} from "../components";
 import { fetchCars } from "@/utils";
 import { fuels, yearsOfProduction } from "@/constants";
 
@@ -36,6 +42,10 @@ export default async function Home({ searchParams }) {
                 <CarCard car={car} />
               ))}
             </div>
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
         ) : (
           <div className="home__error-container">
