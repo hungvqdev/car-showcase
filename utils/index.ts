fb71ca90ea42af1be3a82f70f87bbf9073e59224
Hung VQ <hungvq.dev@gmail.com>
2023-06-11 17:58:47 +0700

    Rapid API Cars

---
+++ utils/index.ts
@@ -0,0 +1,32 @@
+export async function fetchCars() {
+  const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "4745879865msh02cfffd629e9964p1e501bjsnebcc43d5e2a2",
+      "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+    },
+  };
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
