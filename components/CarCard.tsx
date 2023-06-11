0d276776014e7e36ccca526e6446beff97d4e094
Hung VQ <hungvq.dev@gmail.com>
2023-06-11 18:35:12 +0700

    Car Details

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,6 +5,7 @@ import { calculateCarRent } from "@/utils";
 import Image from "next/image";
 import React, { useState } from "react";
 import CustomButton from "./CustomButton";
+import CarDetails from "./CarDetails";
 
 interface CarCardProps {
   car: CarProps;
@@ -85,6 +86,11 @@ const CarCard = ({ car }: CarCardProps) => {
           />
         </div>
       </div>
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
     </div>
   );
 };
